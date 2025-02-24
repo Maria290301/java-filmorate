bf735a9f317057552f3d0a2bd240ffe3fe543cc9
Maria290301 <mezhelevskayamaria@yandex.ru>
2025-02-24 15:55:44 +0200

    Проект 11 Добавлены изменения

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,53 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Slf4j
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final HashMap<Integer, Film> films = new HashMap<>();
+    private int counter = 0;
+
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(++counter);
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film);
+        return film;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new FilmNotFoundException("Фильм с id " + film.getId() + " не найден");
+        }
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм: {}", film);
+        return film;
+    }
+
+    @Override
+    public Film getFilmById(int id) {
+        return films.get(id);
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @Override
+    public void deleteFilm(int id) {
+        if (films.remove(id) == null) {
+            throw new FilmNotFoundException("Фильм с id " + id + " не найден");
+        }
+        log.info("Удалён фильм с id {}", id);
+    }
+}
