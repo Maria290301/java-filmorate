d630bf27abe21b1335b9abd8579da8b1fa7c2e15
Maria290301 <mezhelevskayamaria@yandex.ru>
2025-02-24 16:22:21 +0200

    Проект 11 Добавлены изменения

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
@@ -7,14 +7,15 @@ import java.util.Map;
 
 @Data
 public class ErrorResponse {
-  private String message;
-  private Map<String, String> fieldErrors;
+    private String message;
+    private Map<String, String> fieldErrors;
 
-  public ErrorResponse(String message) {
-    this.message = message;
-    this.fieldErrors = new HashMap<>();
-  }
-  public void addFieldError(String field, String error) {
-    this.fieldErrors.put(field, error);
-  }
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.fieldErrors = new HashMap<>();
+    }
+
+    public void addFieldError(String field, String error) {
+        this.fieldErrors.put(field, error);
+    }
 }
