74ab78df96434d689017e2b961f30008c8354796
Maria290301 <mezhelevskayamaria@yandex.ru>
2025-02-25 23:43:16 +0200

    Проект 11

--- src/main/java/ru/yandex/practicum/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/storage/FilmStorage.java
@@ -4,10 +4,14 @@ import ru.yandex.practicum.model.Film;
 
 import java.util.List;
 
-public interface FilmStorage{
+public interface FilmStorage {
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     Film getFilmById(int id);
+
     List<Film> getAllFilms();
+
     void deleteFilm(int id);
 }
