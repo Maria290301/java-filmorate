ca57e8cae75e30bf4b31ce8e4b5cded5a7c54050
Maria290301 <mezhelevskayamaria@yandex.ru>
2025-02-26 11:49:26 +0200

    Проект 11 добавлены изменения

--- src/main/java/ru/yandex/practicum/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/exception/GlobalExceptionHandler.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Collections;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
